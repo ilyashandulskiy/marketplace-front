6061aeb056793d6e938417af42817bba4bc50450
Ilya Shandulskiy <ilyashandulskiy@gmail.com>
2023-01-23 17:00:06 +0500

    feat: next.js

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Rating } from '../Rating'
-import { useNavigate } from 'react-router'
+import {Rating} from '../Rating'
+import {useRouter} from "next/router";
 
 interface Props {
   id: string
@@ -8,17 +8,18 @@ interface Props {
   price: string
   rating: number
   imageUrl: string
+  onClick: () => void
 }
 
-export const ProductCard = ({ title, price, rating, imageUrl, id }: Props) => {
-  const navigate = useNavigate()
+export const ProductCard = ({ title, price, rating, imageUrl, id, onClick }: Props) => {
 
-  const onClick = () => {
-    navigate('/product/' + id)
-  }
+  const router = useRouter();
 
   return (
-    <div onClick={onClick} className="shadow-xl w-52 h-80 bg-white hover:scale-105 transition rounded overflow-hidden cursor-pointer">
+    <div
+      onClick={onClick}
+      className="shadow-xl w-52 h-80 bg-white hover:scale-105 active:scale-95 transition rounded overflow-hidden cursor-pointer"
+    >
       <div className="bg-gray-300 w-full h-52" />
       <div className="w-full flex flex-row justify-evenly">
         <h1 className="text-xl text-center p-2">{price} руб</h1>
--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -1,15 +1,13 @@
 import React from 'react'
-// @ts-expect-error
-import star from '../../assets/images/star.png'
 
 export const Rating = () => {
   return (
     <div className="w-20 flex flex-row justify-between pt-3">
-      <img src={star} className="w-3 h-3" />
-      <img src={star} className="w-3 h-3" />
-      <img src={star} className="w-3 h-3" />
-      <img src={star} className="w-3 h-3" />
-      <img src={star} className="w-3 h-3" />
+      <img src="/images/star.png" alt="star" className="w-3 h-3" />
+      <img src="/images/star.png" alt="star" className="w-3 h-3" />
+      <img src="/images/star.png" alt="star" className="w-3 h-3" />
+      <img src="/images/star.png" alt="star" className="w-3 h-3" />
+      <img src="/images/star.png" alt="star" className="w-3 h-3" />
     </div>
   )
 }
